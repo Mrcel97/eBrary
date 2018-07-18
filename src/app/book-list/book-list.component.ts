6d1b8fef8d3ba7ea3a2f3c9cf7f87ec98e7734fc
Marcel Ortiz Sanchez <9719marcel@gmail.com>
2018-07-18 10:16:17 +0200

    Added book componenet future functionalities.

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
+import { LibraryComponent } from '../library/library.component';
+import { GoogleBooksService } from '../shared/google-books.service';
 
 @Component({
   selector: 'app-book-list',
@@ -10,6 +13,23 @@ export class BookListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getBook(bookId: string) {
+    //TODO
+  }
+
+  hasBook(book: Book): boolean {
+    //TODO
+    return true;
+  }
+
+  addBook(book: Book) {
+    //TODO
+  }
+
+  removeBook(book: Book) {
+    //TODO
   }
 
 }
