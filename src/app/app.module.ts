123ba299e1e5efde9898c67a1f9114c0fdb795ba
Marcel Ortiz Sanchez <9719marcel@gmail.com>
2018-07-19 15:16:06 +0200

    Added Angular Material.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { GoogleBooksService } from './shared/google-books.service';
 import { LibraryService } from './shared/library.service';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     ReactiveFormsModule,
     HttpModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [GoogleBooksService, LibraryService],
   bootstrap: [AppComponent]
