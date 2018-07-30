d68094fb1b141d3ef63fb360867f07b0ac7c12e4
Marcel Ortiz Sanchez <9719marcel@gmail.com>
2018-07-30 17:30:23 +0200

    Completed page show issue.

--- src/app/pager/pager.component.ts
+++ src/app/pager/pager.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GoogleBooksService } from '../shared/google-books.service';
+import { Page } from './page';
 
 @Component({
   selector: 'app-pager',
@@ -8,8 +9,9 @@ import { GoogleBooksService } from '../shared/google-books.service';
 })
 export class PagerComponent implements OnInit {
   private totalPages: number;
-  private actualPage: number = 1;
   public searchFinish: boolean = false;
+  private actualPage: number = 1;
+  pager: Page[] = [];
 
   constructor(
     private googleBooksService: GoogleBooksService,
@@ -27,20 +29,20 @@ export class PagerComponent implements OnInit {
 
   initPager() {
     this.totalPages = this.googleBooksService.totalPages;
+    for (let page = this.actualPage; page <= 9; page++) { //Total Pages except of page 0 (no resusts on it)
+      this.pager.push( new Page(page) );
+    }
   }
 
   getPage(num: number) {
     const minPages = 0;
     const maxPages = 10;
-
     if ( num < 0 && num > this.totalPages ) {
       return;
     }
     // var > const
     if (minPages < num && num < this.totalPages) {
-      if (num + maxPages < this.totalPages) {
-        this.actualPage = num;
-      }
+      this.actualPage = num;
       this.googleBooksService.changePage(num);
     }
   }
